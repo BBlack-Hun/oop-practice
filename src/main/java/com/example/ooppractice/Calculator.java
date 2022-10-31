596bcfa3f2c45de6301d403e2c5883e33d601640
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-10-31 22:42:49 +0900

    더하기 뺴기 구현 및 테스트 진행

---
+++ src/main/java/com/example/ooppractice/Calculator.java
@@ -0,0 +1,12 @@
+package com.example.ooppractice;
+
+public class Calculator {
+    public static int calculator(int operand1, String operator, int operand2) {
+        if (operator.equals("+")) {
+            return operand1 + operand2;
+        } else if (operator.equals("-")) {
+            return operand1 - operand2;
+        }
+        return 0;
+    }
+}
