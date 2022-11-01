219e720487bf07faee320a9986f6a855f7c609d6
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-01 09:40:35 +0900

    사칙연산 계산기 열거형으로 구현 및 테스트

--- src/test/java/com/example/ooppractice/CalculatorTest.java
+++ src/test/java/com/example/ooppractice/CalculatorTest.java
@@ -1,9 +1,18 @@
 package com.example.ooppractice;
 
+
+import org.assertj.core.util.Streams;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 /**
  * 요구사항
@@ -17,22 +26,22 @@ public class CalculatorTest {
     // 1 + 2 ----> Calculator
     //   3   <----
     @DisplayName("덧셈 연산을 수행한다.")
-    @Test
-    void addictionTest() {
+    @ParameterizedTest
+    @MethodSource("formulaAndResult")
+    void CalculateTest(int operand1, String operator, int operand2, int result) {
 
-        int result = Calculator.calculator(1, "+", 2);
+        int calculatorResult= Calculator.calculator(operand1, operator, operand2);
 
-        assertThat(result).isEqualTo(3);
+        assertThat(calculatorResult).isEqualTo(result);
 
     }
 
-    @DisplayName("뺄셈 연산을 수행한다.")
-    @Test
-    void subTractionTest() {
-
-        int result = Calculator.calculator(1, "-", 2);
-
-        assertThat(result).isEqualTo(-1);
-
+    private static Stream<Arguments> formulaAndResult() {
+        return Stream.of(
+                arguments(1, "+", 2, 3),
+                arguments(1, "-", 2, -1),
+                arguments(4, "*", 2, 8),
+                arguments(4, "/", 2, 2)
+        );
     }
 }
